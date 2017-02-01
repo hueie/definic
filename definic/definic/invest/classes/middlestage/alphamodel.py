604f13b00a13f56963ed822d246fd95959f2928e
kait <kait@kait-PC>
2017-02-01 15:07:06 +0900

    add patch

--- definic/definic/invest/classes/middlestage/alphamodel.py
+++ definic/definic/invest/classes/middlestage/alphamodel.py
@@ -14,7 +14,9 @@ import statsmodels.tsa.stattools as ts
 class AlphaModel():
     def __init__(self):
         self.dbhandler = DBHandler()
-
+        self.window_size = 3 # Alpha Constant
+        self.threshold = 1.5 # Alpha Constant
+        
     def determinePosition(self,code,df,column,row_index,verbose=False):
         pass
 
@@ -52,24 +54,22 @@ class MeanReversionModel(AlphaModel):
         return half_life
 
 
-    def determinePosition(self,df,column,row_index,verbose=False):
-        current_price = df.loc[row_index,column]
+    def determinePosition(self,df, chk_row_idx ,verbose=False):
+        priceSeries = pd.Series(df) 
+        current_price = priceSeries[chk_row_idx]
 
-        df_moving_average = pd.rolling_mean(df.loc[0:row_index,column],window=self.window_size)
-        df_moving_average_std = pd.rolling_std(df.loc[0:row_index,column],window=self.window_size)
+        df_moving_average = priceSeries.rolling(window=self.window_size).mean()
+        df_moving_average_std = priceSeries.rolling(window=self.window_size).std()
 
-        #print df.loc[0:row_index,column]
-        #print moving_average
-        #print df_moving_average_std
-        moving_average = df_moving_average[row_index]
-        moving_average_std = df_moving_average_std[row_index]
+        moving_average = df_moving_average[chk_row_idx]
+        moving_average_std = df_moving_average_std[chk_row_idx]
 
-        price_arbitrage = current_price - moving_average
+        price_arbitrage = float(current_price) - float(moving_average)
 
         if verbose:
             print("diff=%s, price=%s, moving_average=%s, moving_average_std=%s" % (price_arbitrage,current_price,moving_average,moving_average_std))
 
-        if abs(price_arbitrage) > moving_average_std*self.threshold:
+        if abs(price_arbitrage) > moving_average_std * self.threshold:
             
             if np.sign(price_arbitrage)>0:
                 return SHORT
@@ -78,7 +78,7 @@ class MeanReversionModel(AlphaModel):
 
         return HOLD
 
-
+'''
 class MachineLearningModel(AlphaModel):
     def calcScore(self,split_ratio=0.75,time_lags=10):
         return self.predictor.trainAll(split_ratio=split_ratio,time_lags=time_lags )
@@ -107,16 +107,19 @@ class MachineLearningModel(AlphaModel):
             return LONG
         else:
             return SHORT
-
+'''
+   
 if __name__ == "__main__":
     alphamodel = MeanReversionModel()
     
     sql = "SELECT date, close FROM definic.data_stock_usa WHERE stock_code = 'GOOG' order by date"
 
     df = pd.read_sql(sql, alphamodel.dbhandler.conn, index_col='date')
+    df2 = pd.read_sql(sql, alphamodel.dbhandler.conn)
     x = alphamodel.calcHalfLife(np.array(df['close']))
     
     y = alphamodel.calcHurstExponent(np.array(df['close']), 4)
     z = alphamodel.calcADF(np.array(df['close']))
+    sign = alphamodel.determinePosition(np.array(df['close']), len(df2)-1 )
     
-    print(x, y, z)
+    print(x, y, z, sign)
